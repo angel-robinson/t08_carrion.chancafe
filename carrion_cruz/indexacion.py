f4f33ae04339b63af4402f2db6da083da3f7ce42
angel-robinson <57021550+angel-robinson@users.noreply.github.com>
2019-12-13 10:45:42 -0500

    se crearon los 20 ejercicios de indexacion

---
+++ carrion_cruz/indexacion.py
@@ -0,0 +1,147 @@
+# ejemplo 01
+#              1         2
+#    012345678901234567890
+msg="hola robin como estas"
+print(msg[-1])                        # se imprime: s
+print(msg[5])                         # se imprime: r
+print(msg[13],msg[16],msg[20],msg[3]) # se imprime: m e s a
+
+# ejemplo 02
+#               1         2         3
+#     012345678901234567890123456789012
+text="me gusta el curso de programacion"
+print(text[3])                                                  # se imprime: g
+print(text[10])                                                 # se imprime: l
+print(text[0],text[4],text[15],text[30],text[12],text[7])       # se imprime: m u s i c a
+
+# ejemplo 03
+#              1          2
+#    012345678901 234567890 1
+str="electronica \'la mejor\'"
+print(str[12])                                  # se imprime: '
+print(str[-15])                                 # se imprime: n
+print(str[12],str[10],str[7],str[14],str[21])   # se imprime: ' a n a '
+
+# ejemplo 04
+#               1         2
+#     012345678901234567890123456789
+msg1="chiclayo, ciudad de la amistad"
+print(msg1[8])                      # se imprime: ,
+print(msg1[0])                      # se imprime: c
+print(msg1[24],msg1[11],msg1[7])    # se imprime: m i o
+
+# ejemplo 05
+#              1         2
+#    012345678901234567890
+fut="me gusta jugar futbol"
+print(fut[9])                               # se imprime: j
+print(fut[15])                              # se imprime: f
+print(fut[-8],fut[-2],fut[-21],fut[-14])    # se imprime: r o m a
+
+#ejemplo 06
+#               1         2
+#     012345678901234567890123456789
+msg2="el universo de las matematicas"
+print(msg2[-4])                                           # se imprime: i
+print(msg2[-13])                                          # se imprime: s
+print(msg2[3],msg2[4],msg2[26],msg2[27],msg2[10])        # se imprime: u n i c o
+
+#ejemplo 07
+#               1         2
+#     01234567890123456789012345
+msg3="la pedro me cambio la vida"
+print(msg3[3])                                      # se imprime: p
+print(msg3[14])                                     # se imprime: m
+print(msg3[6],msg3[13],msg3[14],msg3[15],msg3[7])   # se imprime: r a m b o
+
+# ejemplo 08
+#               1         2         3
+#     012345678901234567890123456789012345678
+msg4="nos podemos ver en el parque, por favor"
+print(msg4[4])                                                      # se imprime: p
+print(msg4[28])                                                     # se imprime: ,
+print(msg4[4],msg4[7],msg4[24],msg4[6],msg4[37],msg4[0],msg4[0])    # se imprime: p e r d o  n n
+
+# ejemplo 09
+#               1         2         3
+#      01234567890123456789012345678901234567 8
+msg5="\"UNIVERSIDAD NACIONAL PEDRO RUIZ GALLO\""
+print(msg5[38])                                                      # se imprime: "
+print(msg5[22])                                                      # se imprime: P
+print(msg5[0],msg5[2],msg5[29],msg5[23],msg5[4],msg5[37],msg5[38])   # se imprime:" N U E V O "
+
+# ejemplo 10
+#               1         2         3
+#     0123456789012345678901234567890123456789
+msg6="facultad de ciencias fisica y matematica"
+print(msg6[30],msg6[18],msg6[4],msg6[6])         # se imprime: m a l a
+print(msg6[21],msg6[1],msg6[4],msg6[19],msg6[39]) # se imprime: f a l s a
+
+#ejemplo 11
+#               1         2         3
+#     012345678901234567890123456789012345678
+msg7="\'CAMARON QUE SE DUERME SE LO LLEVA LA CORRIENTE\'"
+
+#ejemplo 12
+#               1         2         3
+#     01234567890123456789012345678901234567
+msg8="cuando el tigre pasa los perros ladran"
+print(msg8[16])                                                 # se imprime: p
+print(msg8[37])                                                 # se imprime: n
+print(msg8[0],msg8[1],msg8[17],msg8[10],msg8[27],msg8[29]
+      ,msg8[24],msg8[25],msg8[17],msg8[10],msg8[19],msg8[30])   # se imprime: c u a t r o  p a t a s
+
+#ejemplo 13
+#               1         2         3
+#     01234567890123456789012345678901
+text="peru un pais lleno de maravillas"
+print(text[22],text[9],text[27],text[11],text[11],text[11])         # se imprime: m a i s s s
+print(text[2],text[5],text[11],text[27],text[9])                    # se imprime: r u s i a
+print(text[2],text[23],text[22],text[9],text[11])                   # se imprime: r a m a s
+
+#ejemplo 14
+#               1         2         3         4
+#     01234567890123456789012345678901234567890
+msg9="soy angel, estudio ingenieria electronica"
+print(msg9[6],msg9[22],msg9[37],msg9[16],msg9[4],msg9[8])          # se imprime: g e n i a l
+print(msg9[0],msg9[16],msg9[31],msg9[31],msg9[4],msg9[0])          # se imprime: s i l l a s
+print(msg9[33],msg9[4],msg9[-4],msg9[-1],msg9[8])                  # se imprime: c a n a l
+
+#ejemplo 15
+#               1         2
+#     012345678901234567890123456789
+canc="no puedo dejar de pensar en ti"
+print(canc[3],canc[10],canc[0],canc[22])                                # se imprime: p e n a
+print(canc[-1],canc[-10],canc[5],canc[13],canc[-2],canc[12],canc[23])   # se imprime: i n s e r t a r
+print(canc[9],canc[22],canc[20],canc[21],canc[12],canc[13])             # se imprime: d a n s a r
+
+
+#ejepmlo 16
+#               1         2
+#     01234567890123456789012345
+ejem="robincarrioncruz@gmail.com"
+
+#ejemplo 17
+#               1         2         3
+#     0123456789012345678901234567890
+ejem1="PIMENTEL, LA PLAYA MAS HERMOSA"
+
+#ejemplo 18
+#               1         2         3
+#     0123456789012345678901234567890123456789
+ejem2="\'MACCHU PICCHU\' (maravilla del mundo)"
+
+#ejemplo 19
+#               1         2         3
+#     0123456789012345678901234567890123456
+ejem3="PERU CLASIFICO AL MUNDIAL EN EL 2018"
+
+#ejemplo 20
+#               1         2         3
+#     0123456789012345678901234567890123456789012345678
+ejem4="soy pedro, tengo 20 años y postulo a electronica"
+
+#ejemplo 21
+#               1         2
+#     01234567890123456789012345678
+fin="fin del programa de indexacion"
