b7bd55034a4f5c244501cd268b53c18e973828b8
angel-robinson <57021550+angel-robinson@users.noreply.github.com>
2019-12-13 10:46:28 -0500

    se crearon los 15 ejercicios de longitud

---
+++ carrion_cruz/longitud.py
@@ -0,0 +1,80 @@
+# ejemplos de longitud
+# ejemplo 1
+msg="hey, como estas"   # longitud= 15
+long1=len(msg)
+print("la longitud es:",long1)
+
+# ejemplo 02
+text="\"cuales son tus metas por cumplir\'"        # longitud = 34
+print(len(text))
+
+
+#ejemplo 03
+msg1="el valor de la variable es : \"HELLO\""       # longitud = 36
+long2=len(msg1)
+print("la longitud de la variable msg1 es:",long2)
+
+# ejemplo 04
+msg2="juan, es el hermano mayor"                    # longitud = 25
+long2= len(msg2)
+print("la longitud de la cadenamsg2 es:",long2)
+
+# ejemplo 05
+msg="el poder esta en el querer hacerlo "           # longitud = 35
+long= len(msg)
+print("el mensaje de la cadena msg es:",long)
+
+# ejemplo 06
+msg="tengo muchas ganas de tenerte"                 # longitud = 29
+long= len(msg)
+print("la longitud de la cadena msg es:",long)
+
+#ejemplo 07
+text="\'DICEN QUE LAS ESTRELLAS SE PARECEN A TI\'"  # lomgitud = 41
+long= len(text)
+print(" la longitud de la cadena text es:",long)
+
+# ejemplo 08
+nombre="angel robinson carrion cruz"                # longitud = 27
+print(len(nombre))
+
+# ejemplo 09
+text="la vida de los progamadores"                  # longitud = 27
+print(len(text))
+
+# ejemplo 10
+canc="el mejor deporte es el que te gusta"           # longitud = 35
+long= len(canc)
+print("la longitud de la cadena canc es:",long)
+
+# ejemplo 11
+program="los modales forman al hombre"              # longitud = 28
+long= len(program)
+print("la longitud de la cadena program es:",long)
+
+# ejemplo 12
+msg="mi abuelo tiene 100 años"                      # longitud = 24
+print(len(msg))
+
+# ejemplo 13
+msg="el mundo es ancho y ajeno"
+long= len(msg)
+print("la longitud de la variable es:",long)
+
+# ejemplo 14
+msg="las aventuras de robin"
+long= len(msg)
+print(" la cadena msg tiene longitud:",long)
+
+# ejemplo 15
+msg="tres metro sobre el cielo"
+long= len(msg)
+print(" msg, tiene como longitud:",long)
+
+# ejemplo 16
+msg="PERDONA Y SE TE LLAMO AMOR"        # longitud = 26
+print(len(msg))
+
+# ejemplo 17
+msg="\'\'"                      # longitud = 2
+print(len(msg))
