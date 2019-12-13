3207458aea9174c2142788bf7bf5f7d02ef9ec57
angel-robinson <57021550+angel-robinson@users.noreply.github.com>
2019-12-13 10:46:01 -0500

    se crearon los 15 ejercicios de iteracion

---
+++ carrion_cruz/iteracion.py
@@ -0,0 +1,77 @@
+# iteracion
+# ejemplo 01
+msg1="angel"
+for text in msg1:
+    print(text)
+
+# ejemplo 02
+msg2="PERU ES MI PAIS"
+for text2 in msg2:
+    print(text2)
+
+# ejemplo 03
+correo="acarrion@unprg.edu.pe"
+for letra in correo:
+    print(letra)
+
+# ejemplo 04
+str="\'ella baila como nunca\'"
+for letra2 in str:
+    print(letra2)
+
+# ejemplo 05
+text="EJERCITO DEL PERU"
+for letra in text:
+    print(letra)
+
+# ejemplo 06
+msg="MARINA DE LOS ESTADOS UNIDOS"
+for letra in msg:
+    print(letra)
+
+# ejemplo 07
+msg="las siete maravillas del mundo"
+for letra in msg:
+    print(letra)
+
+# ejemplo 08
+text="MERRY CHRYSMAT"
+for msg in text:
+    print(msg)
+
+# ejemplo 09
+fut="FUTBOL ES EL MEJOR DEPORTE"
+for men in fut:
+    print(men)
+
+# ejemplo 10
+ven="INVASION VENEZOLANA"
+for letra in ven:
+    print(letra)
+
+# ejemplo 11
+text="CARRION CRUZ"
+for msh in text:
+    print(msh)
+
+# ejemplo 12
+mais="el pollo come mais"
+for tet in mais:
+    print(tet)
+
+# ejemplo 13
+tre="LOS TRES CHANCHITOS"
+for msg in tre:
+    print(msg)
+
+# ejemplo 14
+masr="SIETE MARAVILLAS DEL MUNDO"
+for siete in masr:
+    print(siete)
+
+# ejemplo 15
+lov="TU ERES LA LUZ"
+for letra in lov:
+    print(letra)
+
+# terminó, iteracion
