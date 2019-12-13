e56a45cb99f7d8fee19ccc3b765a18576941a256
angel-robinson <57021550+angel-robinson@users.noreply.github.com>
2019-12-13 10:43:17 -0500

    se crearon los 15 ejercicios de comparacion

---
+++ carrion_cruz/comparacion.py
@@ -0,0 +1,72 @@
+# comparacion
+# ejemplo 1
+msg="hola, juan"
+text="hola"
+print(msg==text)            # resultado= False
+
+# ejemplo 02
+msg=" donde estas?"
+resp="no estoy"
+print(msg!=resp)            # resultado= True
+
+# ejemplo 03
+nota="soy robin"
+text="hola"
+print(nota>text)            # resultado= True
+
+# ejemplo 04
+msg="mi nombre es"
+msg1="hola que tal"
+print(msg==msg1)            # resultado= False
+
+# ejemplo 05
+msg="HOLA ROBIN"
+msg1="hola robin"
+print(msg<msg1)            # resultado= True
+
+# ejemplo 06
+msg="EL PODER DE DIOS"
+msg1="hola"
+print(msg<msg1)            # resultado= True
+
+# ejemplo 07
+msg="mi pais peru"
+msg1="desde que aprendi, jamas deje de hacerlo"
+print(msg<msg1)            # resultado= False
+
+# ejemplo 08
+print("te amo"=="te quiero")            # resultado= False
+
+# ejemplo 09
+msg="amante"
+msg1="esposa"
+print(msg>msg1)            # resultado= False
+
+# ejemplo 10
+msg="fea"
+msg1="hermosa"
+print(msg<msg1)            # resultado= True
+
+# ejemplo 11
+text="==>:"
+msg="hola,juan como estas"
+print(text<msg)            # resultado= True
+
+# ejemplo 12
+text="angel"
+print(text=="angel carrion")            # resultado= False
+
+# ejemplo 13
+codigo="ingrese codigo"
+codigo1="192022i"
+print(codigo>codigo1)            # resultado= True
+
+# ejemplo 14
+msg="ingrese mes"
+canc="ESA CANCION ES LA MEJOR"
+print(canc>msg)            # resultado= False
+
+# ejemplo 15
+msg="soy el profesor"
+text="no soy el profesor"
+print(text==msg)            # resultado= False
